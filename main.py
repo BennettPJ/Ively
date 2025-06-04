3e0357552a2a9eeb489899dfebb4230f463d4abc
Bennett Jackson <bennettp.jackson@gmail.com>
2025-06-03 23:50:01 -0700

    up and running

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def shop():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
